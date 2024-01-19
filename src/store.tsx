c0161cd470787ec61324008e9ebeea45c468e6d0
Kolte <v.sitaram.kolte@accenture.com>
2024-01-19 22:11:24 +0530

    Updates

--- src/store.tsx
+++
@@ -1,8 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "./Reducers";
-
-const store = createStore(rootReducer);
-
-export default store;
-export type RootState = ReturnType<typeof rootReducer>;
-export type AppDispatch = typeof store.dispatch;
